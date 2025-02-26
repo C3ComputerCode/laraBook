5c16bfde9bf36dab27573c1a5aca8f2bd2ff73df
C3ComputerCode <c3computercentre@gmail.com>
2025-02-26 16:47:02 +0630

    Books

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use DateTime;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'book_id' => $this->faker->sentence,
+            'name' =>$this->faker->name,
+            'auther' =>$this->faker->name,
+            'total_copies' =>rand(1,5),
+            'available_copies'=>0,            
+            'book_category_id'=>rand(1,5),
+            'publication_date'=>$this->faker->date('Y_m_d'),            
+            
+        ];
+    }
+}
---
+++ database/migrations/2025_02_26_085519_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();            
+            $table->string('book_id');
+            $table->string('name');
+            $table->string('auther');
+            $table->integer('total_copies');
+            $table->integer('available_copies')->nullable();
+            $table->integer('book_category_id')->nullable();
+            $table->date('publication_date')->nullable();
+            $table->integer('photo_id')->nullable();     
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
